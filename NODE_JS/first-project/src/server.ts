6dfbac0fb629df922f7b9c40f0287bc3a7caf89a
Israfil <mdronnieronniers@gmial.com>
2023-11-19 01:51:38 +0600

    project init

---
+++ NODE_JS/first-project/src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+async function main() {
+	try {
+		await mongoose.connect(config.database_url as string);
+		console.log('Connected to database');
+		app.listen(config.port, () => {
+			console.log(`Server running on port`, config.port);
+		});
+	} catch (error) {
+		console.log('connection faild');
+	}
+}
+
+main();
