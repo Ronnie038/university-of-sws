a541cb7f74aa75a4179454b546850c86bc6d993f
Israfil <mdronnieronniers@gmial.com>
2023-11-18 21:36:45 +0600

    init

--- NODE_JS/node_express_typescript/dist/app.js
+++ NODE_JS/node_express_typescript/dist/app.js
@@ -5,5 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 5000;
+// parser
+app.use(express_1.default.json());
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+// if we send text from client then we need express.text() method
+app.use(express_1.default.text());
+app.get('/', logger, (req, res) => {
+    res.send('hello from behind');
+});
 exports.default = app;
--- NODE_JS/node_express_typescript/src/app.ts
+++ NODE_JS/node_express_typescript/src/app.ts
@@ -1,7 +1,21 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 
-const port = 5000;
+// parser
+
+app.use(express.json());
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+	console.log(req.url, req.method, req.hostname);
+	next();
+};
+
+// if we send text from client then we need express.text() method
+app.use(express.text());
+
+app.get('/', logger, (req: Request, res: Response) => {
+	res.send('hello from behind');
+});
 
 export default app;
