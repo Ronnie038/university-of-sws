6dd87dd9ee8e2e40ed005a6bcf8e0f4098c8be4a
Israfil <mdronnieronniers@gmial.com>
2023-12-08 21:08:30 +0600

    _v

--- NODE_JS/first-project/dist/app/utils/cathAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// * Dry principle for do not repeat yourself
-const catchAsync = (fn) => {
-    return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(err => next(err));
-    };
-};
-exports.default = catchAsync;
